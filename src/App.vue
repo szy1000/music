3eb5b75f631d10b0e0cb4f88cdeb4233ac7a9cf7
szy1000 <1538078053@qq.com>
2018-05-07 23:14:34 +0800

    init the file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1>hello</h1>
   </div>
 </template>
 
@@ -11,13 +10,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  h1{
+    font-size: 30px;
+    color: red;
+  }
 }
 </style>
