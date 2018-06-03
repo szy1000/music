f805ea5e7a60dc6be45f89f7f724201a611a71cd
szy1000 <1538078053@qq.com>
2018-06-03 22:44:30 +0800

    add player list

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,21 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <i-player></i-player>
   </div>
 </template>
 
 <script>
   import iHeader from './components/Header/header';
   import iTab from './components/Tab/Tab';
+  import iPlayer from './components/Player/Player';
 
   export default {
     name: 'App',
     components: {
       iHeader,
-      iTab
+      iTab,
+      iPlayer
     }
   }
 </script>
---
+++ src/components/Player/Player.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="player">
+    <div class="normal-player">播放器</div>
+    <div class="mini-player">迷你播放器</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {},
+    data() {
+      return {
+        ratings: [],
+      }
+    },
+    components: {
+    },
+    created() {
+    },
+    events: {},
+    methods: {
+      needShow(type, text) {
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+   @import '~common/scss/variable.scss';
+
+   .player{
+     .normal-player{
+       position: fixed;
+       left: 0;
+       right: 0;
+       top: 0;
+       bottom: 0;
+       z-index: 150;
+       background: $color-background;
+     }
+
+   }
+</style>
