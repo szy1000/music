38aa51325e036fb884a387e02b9bdf899a137796
szy1000 <1538078053@qq.com>
2018-06-05 00:17:24 +0800

    finish the page

--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -2,10 +2,10 @@
   <div class="player" v-show="playlist.length > 0">
     <div class="normal-player" v-show="fullScreen">
       <div class="background">
-        <img width="100%" height="100%">
+        <img width="100%" height="100%" :src="currentSong.image">
       </div>
       <div class="top">
-        <div class="back">
+        <div class="back" @click="back">
           <i class="icon-back"></i>
         </div>
         <h1 class="title" v-html="currentSong.name"></h1>
@@ -15,7 +15,7 @@
         <div class="middle-l" ref="middleL">
           <div class="cd-wrapper" ref="cdWrapper">
             <div class="cd">
-              <img class="image">
+              <img class="image" :src="currentSong.image">
             </div>
           </div>
           <div class="playing-lyric-wrapper">
@@ -23,20 +23,63 @@
           </div>
         </div>
       </div>
-      <div class="bottom"></div>
+      <div class="bottom">
+        <div class="dot-wrapper">
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>
+        <div class="progress-wrapper">
+          <span class="time time-l"></span>
+          <div class="progress-bar-wrapper">
+          </div>
+          <span class="time time-r"></span>
+        </div>
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center">
+            <i class="icon-play"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mini-player" v-show="!fullScreen">迷你播放器</div>
+    <div class="mini-player" @click="open" v-show="!fullScreen">
+      <div class="icon">
+        <img width="40" height="40" :src="currentSong.image">
+      </div>
+      <div class="text">
+        <h2 class="name" v-html="currentSong.name"></h2>
+        <p class="desc" v-html="currentSong.singer"></p>
+      </div>
+      <div class="control">
+        <i class="icon-mini"></i>
+      </div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex';
+  import {mapGetters, mapMutations} from 'vuex';
 
   export default {
     computed: {
       ...mapGetters([
         'fullScreen',
-        'playlist'
+        'playlist',
+        'currentSong'
       ])
     },
     props: {},
@@ -45,6 +88,17 @@
     },
     components: {},
     created() {
+    },
+    methods: {
+      back() {
+        this.setFullScreen(false)
+      },
+      open() {
+        this.setFullScreen(true)
+      },
+      ...mapMutations({
+        setFullScreen: 'SET_FULL_SCREEN'
+      })
     }
   }
 </script>
@@ -87,6 +141,21 @@
             color: $color-theme;
             transform: rotate(-90deg);
           }
+        }
+        .title{
+          width: 70%;
+          margin: 0 auto;
+          line-height: 40px;
+          text-align: center;
+          @include no-wrap();
+          font-size: $font-size-large;
+          color: $color-text;
+        }
+        .subtitle{
+          line-height: 20px;
+          text-align: center;
+          font-size: $font-size-medium;
+          color: $color-text;
         }
       }
       .middle{
@@ -167,83 +236,6 @@
           }
 
         }
-        .bottom{
-          position: absolute;
-          bottom: 50px;
-          width: 100%;
-          .dot-wrapper{
-            text-align: center;
-            font-size: 0;
-            .dot{
-              display: inline-block;
-              vertical-align: middle;
-              margin: 0 4px;
-              width: 8px;
-              height: 8px;
-              border-radius: 50%;
-              background: $color-text-l;
-              &.active{
-                width: 20px;
-                border-radius: 5px;
-                background: $color-text-ll;
-              }
-            }
-
-          }
-          .progress-wrapper{
-            display: flex;
-            align-items: center;
-            width: 80%;
-            margin: 0px auto;
-            padding: 10px 0;
-            .time{
-              color: $color-text;
-              font-size: $font-size-small;
-              flex: 0 0 30px;
-              line-height: 30px;
-              width: 30px;
-              &.time-l{
-                text-align: left;
-              }
-              &.time-r{
-                text-align: right;
-              }
-            }
-            .progress-bar-wrapper{
-              flex: 1
-            }
-          }
-          .operators{
-            display: flex;
-            align-items: center;
-            .icon{
-              flex: 1;
-              color: $color-theme;
-              &.disable{
-                color: $color-theme-d
-              }
-              i{
-                font-size: 30px;
-              }
-            }
-            .i-left{
-              text-align: right;
-            }
-            .i-center{
-              padding: 0 20px;
-              text-align: center;
-              i{
-                font-size: 40px
-              }
-            }
-            .i-right{
-              text-align: left
-            }
-            .icon-favorite{
-              color: $color-sub-theme
-            }
-          }
-        }
         &.normal-enter-active, &.normal-leave-active{
           transition: all 0.4s;
           .top, .bottom{
@@ -257,6 +249,83 @@
           }
           .bottom{
             transform: translate3d(0, 100px, 0)
+          }
+        }
+      }
+      .bottom{
+        position: absolute;
+        bottom: 50px;
+        width: 100%;
+        .dot-wrapper{
+          text-align: center;
+          font-size: 0;
+          .dot{
+            display: inline-block;
+            vertical-align: middle;
+            margin: 0 4px;
+            width: 8px;
+            height: 8px;
+            border-radius: 50%;
+            background: $color-text-l;
+            &.active{
+              width: 20px;
+              border-radius: 5px;
+              background: $color-text-ll;
+            }
+          }
+
+        }
+        .progress-wrapper{
+          display: flex;
+          align-items: center;
+          width: 80%;
+          margin: 0px auto;
+          padding: 10px 0;
+          .time{
+            color: $color-text;
+            font-size: $font-size-small;
+            flex: 0 0 30px;
+            line-height: 30px;
+            width: 30px;
+            &.time-l{
+              text-align: left;
+            }
+            &.time-r{
+              text-align: right;
+            }
+          }
+          .progress-bar-wrapper{
+            flex: 1
+          }
+        }
+        .operators{
+          display: flex;
+          align-items: center;
+          .icon{
+            flex: 1;
+            color: $color-theme;
+            &.disable{
+              color: $color-theme-d
+            }
+            i{
+              font-size: 30px;
+            }
+          }
+          .i-left{
+            text-align: right;
+          }
+          .i-center{
+            padding: 0 20px;
+            text-align: center;
+            i{
+              font-size: 40px
+            }
+          }
+          .i-right{
+            text-align: left
+          }
+          .icon-favorite{
+            color: $color-sub-theme
           }
         }
       }
