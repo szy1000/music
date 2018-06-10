0e168a667d91b7c779eea7506d2331f57d40e158
szy1000 <1538078053@qq.com>
2018-06-10 23:34:19 +0800

    finish the animation

--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -1,78 +1,88 @@
 <template>
   <div class="player" v-show="playlist.length > 0">
-    <div class="normal-player" v-show="fullScreen">
-      <div class="background">
-        <img width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img width="100%" height="100%" :src="currentSong.image">
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <div class="middle">
-        <div class="middle-l" ref="middleL">
-          <div class="cd-wrapper" ref="cdWrapper">
-            <div class="cd">
-              <img class="image" :src="currentSong.image">
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l" ref="middleL">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd">
+                <img class="image" :src="currentSong.image">
+              </div>
+            </div>
+            <div class="playing-lyric-wrapper">
+              <div class="playing-lyric"></div>
             </div>
           </div>
-          <div class="playing-lyric-wrapper">
-            <div class="playing-lyric"></div>
+        </div>
+        <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+          <div class="progress-wrapper">
+            <span class="time time-l"></span>
+            <div class="progress-bar-wrapper">
+            </div>
+            <span class="time time-r"></span>
+          </div>
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
+            </div>
           </div>
         </div>
       </div>
-      <div class="bottom">
-        <div class="dot-wrapper">
-          <span class="dot"></span>
-          <span class="dot"></span>
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" @click="open" v-show="!fullScreen">
+        <div class="icon">
+          <img width="40" height="40" :src="currentSong.image">
         </div>
-        <div class="progress-wrapper">
-          <span class="time time-l"></span>
-          <div class="progress-bar-wrapper">
-          </div>
-          <span class="time time-r"></span>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
         </div>
-        <div class="operators">
-          <div class="icon i-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon i-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-not-favorite"></i>
-          </div>
+        <div class="control">
+          <i class="icon-mini"></i>
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
         </div>
       </div>
-    </div>
-    <div class="mini-player" @click="open" v-show="!fullScreen">
-      <div class="icon">
-        <img width="40" height="40" :src="currentSong.image">
-      </div>
-      <div class="text">
-        <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc" v-html="currentSong.singer"></p>
-      </div>
-      <div class="control">
-        <i class="icon-mini"></i>
-      </div>
-      <div class="control">
-        <i class="icon-playlist"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
   import {mapGetters, mapMutations} from 'vuex';
+  import animations from 'create-keyframe-animation';
 
   export default {
     computed: {
@@ -95,6 +105,55 @@
       },
       open() {
         this.setFullScreen(true)
+      },
+      enter(el, done) {
+        const {x, y, scale} = this._getPosAndScale();
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        }
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+        })
+
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter() {
+        animations.unregisterAnimation('move');
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      leave(el, done) {
+        this.$refs.cdWrapper.style.transition = 'all 0.4s';
+        const {x, y, scale} = this._getPosAndScale();
+        this.$refs.cdWrapper.style.transform = `translate3d(${x}px,${y}px,0) scale(${scale})`;
+        this.$refs.cdWrapper.addEventListener('transitionend', done)
+      },
+      afterLeave() {
+        this.$refs.cdWrapper.style.animation = '';
+        this.$refs.cdWrapper.style.transform = '';
+      },
+      _getPosAndScale() {
+        const targetWidth = 40;
+        const paddingLeft = 40;
+        const paddingBottom = 30;
+        const paddingTop = 80;
+        const width = window.innerWidth * 0.8;
+        const scale = targetWidth / width;
+        const x = -(window.innerWidth / 2 - paddingLeft);
+        const y = (window.innerHeight - paddingTop - width / 2 - paddingBottom);
+        return {x, y, scale}
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN'
@@ -142,7 +201,7 @@
             transform: rotate(-90deg);
           }
         }
-        .title{
+        .title {
           width: 70%;
           margin: 0 auto;
           line-height: 40px;
@@ -151,46 +210,46 @@
           font-size: $font-size-large;
           color: $color-text;
         }
-        .subtitle{
+        .subtitle {
           line-height: 20px;
           text-align: center;
           font-size: $font-size-medium;
           color: $color-text;
         }
       }
-      .middle{
+      .middle {
         position: fixed;
         width: 100%;
         top: 80px;
         bottom: 170px;
         white-space: nowrap;
         font-size: 0;
-        .middle-l{
+        .middle-l {
           display: inline-block;
           vertical-align: top;
           position: relative;
           width: 100%;
           height: 0;
           padding-top: 80%;
-          .cd-wrapper{
+          .cd-wrapper {
             position: absolute;
             left: 10%;
             top: 0;
             width: 80%;
             height: 100%;
-            .cd{
+            .cd {
               width: 100%;
               height: 100%;
               box-sizing: border-box;
               border: 10px solid rgba(255, 255, 255, 0.1);
               border-radius: 50%;
-              &.play{
+              &.play {
                 animation: rotate 20s linear infinite;
               }
-              &.pause{
+              &.pause {
                 animation-play-state: paused;
               }
-              .image{
+              .image {
                 position: absolute;
                 left: 0;
                 top: 0;
@@ -200,12 +259,12 @@
               }
             }
           }
-          .playing-lyric-wrapper{
+          .playing-lyric-wrapper {
             width: 80%;
             margin: 30px auto 0 auto;
             overflow: hidden;
             text-align: center;
-            .playing-lyric{
+            .playing-lyric {
               height: 20px;
               line-height: 20px;
               font-size: $font-size-medium;
@@ -214,52 +273,36 @@
 
           }
         }
-        .middle-r{
+        .middle-r {
           display: inline-block;
           vertical-align: top;
           width: 100%;
           height: 100%;
           overflow: hidden;
-          .lyric-wrapper{
+          .lyric-wrapper {
             width: 80%;
             margin: 0 auto;
             overflow: hidden;
             text-align: center;
-            .text{
+            .text {
               line-height: 32px;
               color: $color-text-l;
               font-size: $font-size-medium;
-              &.current{
+              &.current {
                 color: $color-text
               }
             }
           }
-
-        }
-        &.normal-enter-active, &.normal-leave-active{
-          transition: all 0.4s;
-          .top, .bottom{
-            transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32);
-          }
-        }
-        &.normal-enter, &.normal-leave-to{
-          opacity: 0;
-          .top{
-            transform: translate3d(0, -100px, 0)
-          }
-          .bottom{
-            transform: translate3d(0, 100px, 0)
-          }
         }
       }
-      .bottom{
+      .bottom {
         position: absolute;
         bottom: 50px;
         width: 100%;
-        .dot-wrapper{
+        .dot-wrapper {
           text-align: center;
           font-size: 0;
-          .dot{
+          .dot {
             display: inline-block;
             vertical-align: middle;
             margin: 0 4px;
@@ -267,7 +310,7 @@
             height: 8px;
             border-radius: 50%;
             background: $color-text-l;
-            &.active{
+            &.active {
               width: 20px;
               border-radius: 5px;
               background: $color-text-ll;
@@ -275,58 +318,73 @@
           }
 
         }
-        .progress-wrapper{
+        .progress-wrapper {
           display: flex;
           align-items: center;
           width: 80%;
           margin: 0px auto;
           padding: 10px 0;
-          .time{
+          .time {
             color: $color-text;
             font-size: $font-size-small;
             flex: 0 0 30px;
             line-height: 30px;
             width: 30px;
-            &.time-l{
+            &.time-l {
               text-align: left;
             }
-            &.time-r{
+            &.time-r {
               text-align: right;
             }
           }
-          .progress-bar-wrapper{
+          .progress-bar-wrapper {
             flex: 1
           }
         }
-        .operators{
+        .operators {
           display: flex;
           align-items: center;
-          .icon{
+          .icon {
             flex: 1;
             color: $color-theme;
-            &.disable{
+            &.disable {
               color: $color-theme-d
             }
-            i{
+            i {
               font-size: 30px;
             }
           }
-          .i-left{
+          .i-left {
             text-align: right;
           }
-          .i-center{
+          .i-center {
             padding: 0 20px;
             text-align: center;
-            i{
+            i {
               font-size: 40px
             }
           }
-          .i-right{
+          .i-right {
             text-align: left
           }
-          .icon-favorite{
+          .icon-favorite {
             color: $color-sub-theme
           }
+        }
+      }
+      &.normal-enter-active, &.normal-leave-active {
+        transition: all 0.4s;
+        .top, .bottom {
+          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32);
+        }
+      }
+      &.normal-enter, &.normal-leave-to {
+        opacity: 0;
+        .top {
+          transform: translate3d(0, -100px, 0)
+        }
+        .bottom {
+          transform: translate3d(0, 100px, 0)
         }
       }
     }
